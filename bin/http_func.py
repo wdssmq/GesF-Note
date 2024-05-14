e6896e04f05194524de66d74288fb6242d3b5ae0
wdssmq <wdssmq@qq.com>
2024-05-14 10:37:57 +0800

    ci-py. github api 方法

--- bin/http_func.py
+++ bin/http_func.py
@@ -1,12 +1,14 @@
 """ HTTP 请求 """
+
 import sys
 import inspect
 import requests
+# import json
 
-from bin.base import fnErr
+from bin.base import fnErr, fnLog
 
 
-def http(req_arg=None, data_arg=None, headers_arg=None):
+def http(req_arg=None, data_arg=None, json_arg=None, headers_arg=None):
     """HTTP 请求"""
     if data_arg is None:
         data_arg = {}
@@ -20,10 +22,17 @@ def http(req_arg=None, data_arg=None, headers_arg=None):
             res_info = requests.get(
                 url, params=data_arg, headers=headers_arg, timeout=10
             )
-        else:
+        elif req_arg["method"] == "post":
             res_info = requests.post(
-                url, data=data_arg, headers=headers_arg, timeout=10
+                url, data=data_arg, json=json_arg, headers=headers_arg, timeout=10
             )
+        elif req_arg["method"] == "patch":
+            res_info = requests.patch(
+                url, data=data_arg, json=json_arg, headers=headers_arg, timeout=10
+            )
+        else:
+            fnErr("不支持的请求方法", inspect.currentframe().f_lineno)
+            sys.exit(0)
     except KeyError:
         fnErr("网络错误", inspect.currentframe().f_lineno)
         sys.exit(0)
@@ -45,7 +54,6 @@ def http_git_issues(labels="pick", repo="", token=""):
     url = f"https://api.github.com/repos/{repo}/issues?labels={labels}"
     headers = http_git_headers(token)
     res_data = http({"url": url, "method": "get"}, headers_arg=headers)
-    # fnLog(res_data.json(), inspect.currentframe().f_lineno)
     return res_data.json()
 
 def http_git_issues_comments(comments_url, token=""):
@@ -53,5 +61,12 @@ def http_git_issues_comments(comments_url, token=""):
     url = comments_url
     headers = http_git_headers(token)
     res_data = http({"url": url, "method": "get"}, headers_arg=headers)
-    # fnLog(res_data.json(), inspect.currentframe().f_lineno)
+
     return res_data.json()
+
+
+def http_git_edt_issue(url, data, token=""):
+    """编辑 issue"""
+    headers = http_git_headers(token)
+    res_data = http({"url": url, "method": "patch"}, json_arg=data, headers_arg=headers)
+    fnLog(res_data.json(), inspect.currentframe().f_lineno)
