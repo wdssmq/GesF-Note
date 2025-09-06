fb36912510fc89d57bec3bff90a468148efe445c
wdssmq <wdssmq@qq.com>
2025-09-06 15:34:53 +0800

    up-py. 同仓库提交动态合并

--- bin/git_func.py
+++ bin/git_func.py
@@ -51,13 +51,18 @@ def git_func_event_details(event_list):
         event_type = event.get("type", "")
         if event_type != "PushEvent":
             continue
-        event_id = event.get("id", "")
         event_repo = event.get("repo", {}).get("name", "")
-        details.append({
-            "id": event_id,
-            "type": event_type,
-            "repo": event_repo,
-            "commits": extract_commit_info(event.get("payload", {})),
+        # 判断是否存在同名的 repo 在 details ,存在则合并 commits
+        if any(d["repo"] == event_repo for d in details):
+            for d in details:
+                if d["repo"] == event_repo:
+                    d["commits"].extend(extract_commit_info(event.get("payload", {})))
+                    break
+        else:
+            details.append({
+                "type": event_type,
+                "repo": event_repo,
+                "commits": extract_commit_info(event.get("payload", {})),
         })
     return details
 
@@ -128,6 +133,18 @@ def save_data(data, file_type="yml"):
         f"保存文件：{file_path}", inspect.currentframe().f_lineno
     )
 
+# 解析并保存 event details
+def parse_and_save_event_details():
+    """解析并保存 event details"""
+    # 抓取 events
+    events = git_func_events()
+    # 获取 event 详情
+    details = git_func_event_details(events)
+    if not details:
+        fnBug("未获取到 event 详情数据", inspect.currentframe().f_lineno)
+        return
+    # 保存 event 详情到文件
+    save_event_details(details)
 
 # 主入口函数
 def git_func_main():
