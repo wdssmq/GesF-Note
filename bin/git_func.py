3abb29245b50a2a1e70ce90fadd6c7502afe2143
wdssmq <wdssmq@qq.com>
2026-09-23 14:08:17 +0800

    优化注释，增强代码可读性

--- bin/git_func.py
+++ bin/git_func.py
@@ -10,7 +10,8 @@ from bin.git_func_create_cmt import process_json_files
 def git_func_main():
     """主入口函数"""
     fnBug("git_func_main", fnLineNo())
+    # 解析并保存 GitHub issues 和 events 的详细信息
     parse_and_save_issues_details()
     parse_and_save_event_details()
-
+    # 处理 JSON 文件，生成评论等内容
     process_json_files()
