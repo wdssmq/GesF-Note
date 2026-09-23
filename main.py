b94d4cb07eb64082bf6ff277e2247b437c07e265
wdssmq <wdssmq@qq.com>
2026-09-23 14:43:48 +0800

    修复 debug 配置逻辑，默认为 False

--- main.py
+++ main.py
@@ -49,6 +49,8 @@ def init():
     config_info["MD_PATH"] = os.path.join(os.getcwd(), "blog-astro/src/content/blog/")
     # 最大评论数
     config_info["MAX_NOTES"] = 14
+    # debug 模式
+    config_info["DEBUG"] = False
 
     # 读取 debug 配置
     if "DEBUG" in config_info.keys() and config_info["DEBUG"]:
